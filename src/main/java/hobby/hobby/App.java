623e239a9ed502a31f937630676e163d47476253
jonathansiahaan <jonathan.siahaan@justcommodity.com>
2016-03-05 00:35:32 +0700

    Rev 1
    Rev 1

---
+++ src/main/java/hobby/hobby/App.java
@@ -0,0 +1,13 @@
+package hobby.hobby;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
